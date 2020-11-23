959b561278e9590916b09924e265b19a9c205dc8
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-23 11:39:10 +0530

    Update Routing.js

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -4,6 +4,9 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class Routing extends Component{
     render(){
+        <BrowserRouter>
+        
+        </BrowserRouter>
     }
 }
 
