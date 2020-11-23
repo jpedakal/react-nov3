b02fbb491f1ddcf0c4c4d3b42f266b61967e231b
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-23 11:55:07 +0530

    Update Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+
+    UNSAFE_componentWillMount() {
+
+    }
+
+    UNSAFE_componentWillUpdate() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+    componentWillUnmount() {
+
+    }
+}
+
+export default Home;
