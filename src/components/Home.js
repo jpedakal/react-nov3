e390240a6b83deba5b93b79e2db52cdca6575e4c
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-24 19:52:43 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,41 +2,12 @@ import React, { Component, Fragment } from 'react';
 import Banner from './Banner';
 
 class Home extends Component {
-
-    constructor() {
-        super()
-
-        this.state = {
-
-        }
-    }
-
-    UNSAFE_componentWillMount() {
-
-    }
-
-    UNSAFE_componentWillUpdate() {
-
-    }
-
-    shouldComponentUpdate() {
-
-    }
-
     render() {
         return (
             <Fragment>
                 <Banner />
             </Fragment>
         )
-    }
-
-    componentDidUpdate() {
-
-    }
-
-    componentWillUnmount() {
-
     }
 }
 
