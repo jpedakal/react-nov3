4b2016d28a8e952d2be0042890913932a8c39546
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-25 06:16:01 +0530

    Update ArtistList.js

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ArtistList = (props) => {
+
+    const renderList = ({ artistData }) => {
+        return artistData.map((id, index) => {
+            return (
+                <div>
+                    <Link to="/">
+                        <div>{id.name}</div>
+                    </Link>
+                </div>
+            )
+        })
+    }
     return (
         <div className="artist_list">
-           <h4>List of Artists</h4>
+            <h4>List of Artists</h4>
         </div>
     )
 }
